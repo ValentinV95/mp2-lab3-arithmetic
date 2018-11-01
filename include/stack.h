539c5e40deb09df96f981249df38291ccfddd1f0
unknown <shramek.m@WS-6K-102-A13.ccam.cmc.unn.net>
2018-11-01 14:06:33 +0300

    Stack_1.0

--- include/stack.h
+++ include/stack.h
@@ -7,3 +7,81 @@
 // - получение количества элементов в стеке
 // - очистка стека
 // при вставке в полный стек должна перевыделяться память
+#include "iostream"
+#include "vector"
+using namespace std;
+
+template <class val>
+class Stack
+{
+private:
+	vector <val> p;
+	int Size;
+	int top;
+	bool IsFull();
+public:
+	Stack(int n = 10);
+	~Stack() {};
+	bool empty();
+	val front();
+	void pop();
+	void push(val s);
+};
+
+template <class val>
+Stack<val>::Stack(int n)
+{
+	Size = n;
+	top = -1;
+	p.resize(n);
+}
+template <class val>
+bool Stack<val>::empty()
+{
+	if(top == -1)
+	{
+		return true;
+	}
+	return false;
+}
+
+template <class val>
+bool Stack<val>::IsFull()
+{
+	if (top == Size - 1)
+	{
+		return true;
+	}
+	return false;
+
+}
+
+template <class val>
+void Stack<val>::pop()
+{
+	if(!empty())
+	{
+		top--;
+	}
+	else
+	{
+		cout << "Stack is empty" << endl;
+	}
+}
+
+template <class val>
+void Stack<val>::push(val v)
+{
+	if(IsFull())
+	{
+		Size = 2 * Size;
+		p.resize(Size);
+	}
+	p[++top] = v;
+}
+
+template <class val>
+val Stack<val>::front()
+{
+	return p[top];
+}
