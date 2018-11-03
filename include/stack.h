d987366f74fdb80c14f4e2c8bf24fe4b70e72e9f
Shramek42 <shramek42@mail.ru>
2018-11-03 10:59:50 +0300

    Stack_2.0

--- include/stack.h
+++ include/stack.h
@@ -1,12 +1,3 @@
-// объявление и реализация шаблонного стека
-// стек поддерживает операции: 
-// - вставка элемента, 
-// - извлечение элемента, 
-// - просмотр верхнего элемента (без удаления)
-// - проверка на пустоту, 
-// - получение количества элементов в стеке
-// - очистка стека
-// при вставке в полный стек должна перевыделяться память
 #include "iostream"
 #include "vector"
 using namespace std;
@@ -22,8 +13,9 @@ private:
 public:
 	Stack(int n = 10);
 	~Stack() {};
-	bool empty();
+	bool IsEmpty();
 	val front();
+	int GetSize() {return Size;}
 	void pop();
 	void push(val s);
 };
@@ -36,7 +28,7 @@ Stack<val>::Stack(int n)
 	p.resize(n);
 }
 template <class val>
-bool Stack<val>::empty()
+bool Stack<val>::IsEmpty()
 {
 	if(top == -1)
 	{
@@ -59,13 +51,13 @@ bool Stack<val>::IsFull()
 template <class val>
 void Stack<val>::pop()
 {
-	if(!empty())
+	if(!IsEmpty())
 	{
 		top--;
 	}
 	else
 	{
-		cout << "Stack is empty" << endl;
+		throw("Stack is empty");
 	}
 }
 
@@ -83,5 +75,9 @@ void Stack<val>::push(val v)
 template <class val>
 val Stack<val>::front()
 {
+	if(IsEmpty())
+	{
+		throw("Stack is empty");
+	}
 	return p[top];
 }
