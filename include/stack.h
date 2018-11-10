39ff964def93d065b307c0d2b9415cf683a14a53
ishibyeva <43236369+ishibyeva@users.noreply.github.com>
2018-11-10 18:31:01 +0300

    Add stack and test for stack

--- include/stack.h
+++ include/stack.h
@@ -7,3 +7,98 @@
 // - получение количества элементов в стеке
 // - очистка стека
 // при вставке в полный стек должна перевыделяться память
+
+template <class TType>
+class TStack
+{
+	int top;
+	int size;
+	TType *mas;
+
+public:
+	TStack();
+	TStack(int ssize);
+	void Push(const TType &val);
+	TType Pop();
+	bool Empty();
+	void NewLen();
+	~TStack() { delete[]mas; };
+	TType CheckLast();
+	void ClearStack();
+	int GetSize();
+};
+
+template <class TType>
+TStack <TType>::TStack()
+{
+
+	size = 10;
+	top = -1;
+	mas = new TType[size];
+}
+
+template <class TType>
+TStack <TType>::TStack(int ssize)
+{
+	if (ssize <= 0)
+		throw ("Uncorr stack leight");
+	size = ssize;
+	top = -1;
+	mas = new TType[size];
+}
+template <class TType>
+void TStack<TType>::Push(const TType &val)
+{
+	if (size == top + 1)
+		NewLen();
+	mas[top + 1] = val;
+	top++;
+}
+template <class TType>
+TType TStack<TType>::Pop()
+{
+	if (Empty())
+		throw ("Stack is empty");
+
+	TType a = mas[top];
+	top--;
+	return a;
+
+}
+template <class TType>
+void TStack<TType>::NewLen()
+{
+	TType *Copmas = new TType[size * 2];
+	for (int i = 0; i < size; i++)
+		Copmas[i] = mas[i];
+	delete[]mas;
+	size *= 2;
+	mas = Copmas;
+}
+template <class TType>
+bool TStack<TType>::Empty()
+{
+	if (top == -1)
+		return true;
+	else return false;
+}
+
+template <class TType>
+TType TStack<TType>::CheckLast()
+{
+	if (Empty())
+		 throw ("Stack is empty");
+	return (mas[top]);
+}
+template <class TType>
+void TStack<TType>::ClearStack()
+{
+	top = -1;
+
+
+}
+template <class TType>
+int TStack<TType>::GetSize()
+{
+	return size;
+}
--- test/test_stack.cpp
+++ test/test_stack.cpp
@@ -2,3 +2,101 @@
 
 #include "stack.h"
 #include <gtest.h>
+
+TEST(TStack, can_generate_stack_without_leight)
+{
+	ASSERT_NO_THROW(TStack <int> stk);
+}
+TEST(TStack, can_generate_stack_with_leight)
+{
+	ASSERT_NO_THROW(TStack <int> stk(20));
+}
+TEST(TStack, can_create_stack_with_wrong_leight)
+{
+	ASSERT_ANY_THROW(TStack <int> stk(-20));
+}
+TEST(TStack, new_stack_is_empty)
+{
+	TStack <int> stk(20);
+	EXPECT_EQ(stk.Empty(), true);
+}
+TEST(TStack, can_push_to_empty_st)
+{
+	TStack <int> stk;
+	ASSERT_NO_THROW(stk.Push(3));
+
+}
+TEST(TStack, can_push_and_check)
+{
+	TStack <int> stk;
+	stk.Push(45);
+	EXPECT_EQ(stk.CheckLast(), 45);
+
+}
+TEST(TStack, can_push_and_pop)
+{
+	TStack <int> stk;
+	stk.Push(45);
+	stk.Pop();
+	EXPECT_EQ(stk.Empty(), true);
+}
+
+TEST(TStack, overflow_of_massive)
+{
+	TStack <int> stk(2);
+	stk.Push(1);
+	stk.Push(2);
+	stk.Push(3);
+	ASSERT_NO_THROW(stk.Push(4));
+}
+TEST(TStack, can_pop_empty_stack)
+{
+	TStack <int> stk;
+	ASSERT_ANY_THROW(stk.Pop());
+}
+
+TEST(TStack, check_top_elements)
+{
+	TStack <int> stk(4);
+	stk.Push(1);
+	stk.Push(2);
+	stk.Push(3);
+	stk.Pop();
+	EXPECT_NE(stk.CheckLast(), 3);
+}
+TEST(TStack, check_dif_size_0f_stk)
+{
+	TStack <int> stk(2);
+	stk.Push(1);
+	stk.Push(2);
+	stk.Push(3);
+	stk.Push(4);
+	EXPECT_NE(stk.GetSize(), 2);
+
+}
+TEST(TStack, check_size_of_clear_stk)
+{
+	TStack <int> stk(20);
+	EXPECT_EQ(stk.GetSize(), 20);
+}
+
+TEST(TStack, can_check_last_in_empty)
+{
+	TStack <int> stk;
+	ASSERT_ANY_THROW(stk.CheckLast());
+}
+TEST(TStack, clear_stk)
+{
+	TStack <int> stk;
+	stk.Push(1);
+	stk.Push(1);
+	stk.Push(1);
+	stk.Push(1);
+	stk.Push(1);
+	stk.Push(1);
+	stk.Push(1);
+	stk.ClearStack();
+
+	EXPECT_EQ(stk.Empty(), true);
+
+}
