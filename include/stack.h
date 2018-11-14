7729742c0f8538502b628fc32dc75359d2148f3d
Kudryashov Nikita <porpois60@gmail.com>
2018-11-15 00:00:06 +0300

    stack

--- include/stack.h
+++ include/stack.h
@@ -7,3 +7,72 @@
 // - получение количества элементов в стеке
 // - очистка стека
 // при вставке в полный стек должна перевыделяться память
+
+#include <vector>
+#include <iostream>
+using namespace std;
+#define DEFAULT_SIZE 10
+template <class T> class Stack
+{
+private:
+	T* a;
+	int _size;
+	int top;
+public:
+	Stack()
+	{
+		_size = DEFAULT_SIZE;
+		a = new T[_size];
+		top = -1;
+	}
+	void push(T number)
+	{
+		if (this->is_full())
+		{
+			T* b = new T[2 * _size];
+			_size *= 2;
+			for (int i = 0;i<_size;i++)
+			{
+				b[i] = a[i];
+			}
+			delete[] a;
+			a = b;
+
+		}
+		a[++top] = number;
+	}
+	T pop()
+	{
+		if (this->is_empty())
+		{
+			throw "Stack is empty";
+		}
+		else
+		{
+			return a[top--];
+		}
+	}
+	T top()
+	{
+		return a[top];
+	}
+	bool is_empty()
+	{
+		return top == -1;
+	}
+	bool is_full()
+	{
+		return top == (_size - 1);
+	}
+	int size()
+	{
+		return top + 1;
+	}
+	void clear()
+	{
+		top = -1;
+		delete[] a;
+		a = new T[DEFAULT_SIZE];
+	}
+};
+
