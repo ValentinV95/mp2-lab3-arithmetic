9294d1291d399ccce4df8902b012bd92ce7b2884
ishibyeva <43236369+ishibyeva@users.noreply.github.com>
2018-11-18 02:48:02 +0300

    class with checking

--- include/arithmetic.h
+++ include/arithmetic.h
@@ -1,1 +1,76 @@
 // объ€вление функций и классов дл€ вычислени€ арифметических выражений
+#ifndef arif_H
+#define arif_H
+#include "stack.h"
+#include <string>
+#include <stdio.h>
+#include <math.h>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+TStack <double> Num;
+TStack <char> Op;
+
+
+
+class Lexem
+{
+	char op;
+	int prior;
+	double num;
+	bool notop;
+	bool binary;
+
+public:
+	Lexem(double number) { num = number; notop = true; binary = false; }
+	Lexem(char oper){num = 0; notop = false; op = oper; prior = 0; binary = true;}
+	void Parsing(string a);
+	void Unar()
+	{
+		binary = !(binary);
+	}
+	int GetPrior()
+	{
+		return prior;
+	}
+	void Putprior(int a)
+	{
+		prior = a;
+	}
+	void SetVal(double a)
+	{
+		num = a;
+	}
+	double GetNum()
+	{
+		return num;
+	}
+	char GetOper()
+	{
+		return op;
+	}
+	bool GetType()
+	{
+		return notop;
+	}
+
+
+
+	~Lexem() {};
+	friend ostream& operator<<(ostream &out, const Lexem &lex); 
+
+};
+
+
+vector<Lexem> PrePolish;
+// checking block
+ bool Skobki(string s);
+ bool CheckSequence(string s);
+ bool ValisOne(string s);
+void BlockforString(string s);
+
+
+
+
+#endif
--- src/arithmetic.cpp
+++ src/arithmetic.cpp
@@ -1,1 +1,144 @@
 // реализация функций и классов для вычисления арифметических выражений
+#include "arithmetic.h"
+
+void Lexem::Parsing(string a)
+{
+	int i = 0, k=0;
+	
+	while (a[i]!='\0')
+	{
+		if (a[i] == ' ')
+			i++;
+		else if ((a[i] >= '0') && (a[i] <= '9'))
+		{
+			double dd = 0.0;
+
+			PrePolish.push_back(dd);
+			i++;
+		}
+
+		else if ((a[i] == '+') || (a[i] == '-') || (a[i] == '/') || (a[i] == '(') || (a[i] == ')'))
+		{
+
+			PrePolish.push_back(a[i]);
+			Lexem A = PrePolish.back();
+			PrePolish.pop_back();
+			A.Putprior(2);
+
+			if (a[i] == '-')
+			{
+				if (!(PrePolish.empty()))
+				{
+					Lexem B = PrePolish.back();
+					if (!(B.GetType()))
+						A.Unar();
+					A.Putprior(0);
+				}
+				else {
+					A.Unar();
+					A.Putprior(0);
+				}
+
+			
+	
+				PrePolish.push_back(A);
+				i++;
+			}
+			if ((a[i] == '*') || (a[i] == '/'))
+
+			{
+				A.Putprior(1);
+				PrePolish.push_back(A);
+				i++;
+			}
+			if (a[i] == '+')
+			{
+				A.Putprior(2);
+			PrePolish.push_back(A);
+			i++;
+			}
+		
+		}
+		else if ((a[i] >= 'a') && (a[i] <= 'z'))
+		{
+			PrePolish.push_back(a[i]);
+			i++;
+		}
+	}
+}
+
+
+
+bool Skobki(string s)
+{
+	TStack<char> rez;
+	for (int i = 0; i < (int)s.length(); i++)
+	{
+		if (s[i] == '(' )
+		{
+			rez.Push(s[i]);
+		}
+		else if (s[i] == ')')
+		{
+			if(rez.Empty() || ((s[i] == ')') ^ (rez.CheckLast() == '(')))
+			{
+				cout << "Wrong sintaxis" << endl;
+				return false;
+			}
+			rez.Pop();
+		}
+	}
+
+	return rez.Empty();
+}
+
+bool CheckSequence(string s)
+{
+	if ((s[1] == '*') || (s[1] == '/') || (s[1] == '+'))
+	{
+		cout << "Wrong first element" << endl;
+		return false;
+	}
+	if ((s[s.size() - 1] == '-') || (s[s.size() - 1] == '+') || (s[s.size() - 1] == '*') || (s[s.size() - 1] == '/'))
+	{
+		cout << "Wrong last element" << endl;
+		return false;
+	}
+	for (int i = 0; i < (int)s.length(); i++)
+	{
+		if ((s[i] == '+' || s[i] == '-' || s[i] == '*' || s[i] == '/' || s[i]=='(') && (s[i + 1] == '+' || s[i + 1] == '*' || s[i + 1] == '/' || s[i+1]==')')) {
+			cout << "operation after operation" << endl;
+			return false;
+
+		}
+		if ((s[i] == '/') && (s[i + 1] == '0'))
+		{
+			cout << "division by 0" << endl;
+			return false;
+		}
+	}
+}
+
+
+
+
+bool ValisOne(string s)
+{
+	string qq;
+	for (int i = 0; i < (int)s.length(); i++)
+	{
+		if ((s[i] >= 'a') && (s[i] <= 'z'))
+			qq += s[i];
+	}
+	for (int i = 0; i < (int)qq.length(); i++)
+	{
+		if (qq[1] != qq[i])
+		{
+			cout << "Wrong value name" << endl;
+			return false;
+		}
+	}
+	return true;
+
+}
+
