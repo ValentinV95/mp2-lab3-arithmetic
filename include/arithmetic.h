df376985746280a0565b7be91e8d53e4340f7760
NikolayGromov <43181570+NikolayGromov@users.noreply.github.com>
2018-11-25 18:01:53 +0300

    Add files via upload

--- include/arithmetic.h
+++ include/arithmetic.h
@@ -1,1 +1,78 @@
 // объ€вление функций и классов дл€ вычислени€ арифметических выражений
+#include <string>
+#include "stack.h"
+enum type{oper,num,o_b,c_b};
+class Oper_or_numb
+{
+private:
+	int lvl;
+	char op;
+	type on;
+	double val;
+public:
+	Oper_or_numb();
+	Oper_or_numb(char c);
+	Oper_or_numb(double a);
+	~Oper_or_numb() { lvl = 0; val = 0; };
+	void Set(double a) 
+	{
+		on = num;
+		val = a;
+	}
+	void Set(char c) {
+		on = oper;
+		switch (c)
+		{
+		case '+':
+		{
+			lvl = 1;
+			op = '+';
+			break;
+		}
+		case '-':
+		{
+			lvl = 1;
+			op = '-';
+			break;
+		}
+		case '*':
+		{
+			lvl = 2;
+			op = '*';
+			break;
+		}
+		case '/':
+		{
+			lvl = 2;
+			op = '/';
+			break;
+		}
+		case '(':
+		{
+			op = '(';
+			break;
+		}
+
+		}
+	};;
+	//void Set(type)
+	type Get_type() { return on; }
+	double Get_value() { return val; }
+	int Get_lvl() { return lvl; };
+	char Get_op() { return op; };
+	Oper_or_numb& operator=(Oper_or_numb& a);
+};
+class Lexeme
+{
+private:
+	Stack <double> TStack;
+	char* c;
+	type* kind;
+	int n;
+public:
+	Lexeme( char* c1);
+	type Get_kind(int i) { return kind[i]; };
+	char Get_c(int i) { return c[i]; };
+	double Answer(Oper_or_numb* o);
+	Oper_or_numb* RPN();
+};
--- include/stack.h
+++ include/stack.h
@@ -7,3 +7,69 @@
 // - получение количества элементов в стеке
 // - очистка стека
 // при вставке в полный стек должна перевыделяться память
+
+ 
+#include <stdlib.h>
+#ifndef STACK_H
+#define STACK_H
+template <typename T> class Stack
+{
+private:
+	int n;
+	T *q;
+	int pos;
+public:
+	Stack(int nk=10)
+	{
+		n = nk;
+		q = new T[n];
+		pos = 0;
+	}
+	~Stack()
+	{
+		pos = 0;
+		delete[] q;
+	}
+	int Get_pos() { return pos; }
+	void Resize_IF()
+	{
+		if (pos == n)
+		{
+			T* tmp;
+			tmp = new T[1.5*n];
+			for (int i = n - 1;i >= 0;i--)
+				tmp[i] = Get();
+			delete[] q;
+			q = tmp; 
+			pos = n;
+			n = 1.5*n;
+		}
+	}
+	bool IsEmpty()
+	{
+		if (!(pos))
+			return(true);
+		else
+			return(false);
+	}
+	void Set(T k)
+	{
+		Resize_IF();
+		q[pos++] = k;
+	}
+	T Get()
+	{
+		if (!(IsEmpty()))
+		{
+			pos--;
+			return(q[pos]);
+		}
+		else
+			throw "Stack is empty";
+	}
+	T Top()
+	{
+		return(q[pos-1]);
+	}
+};
+#endif STACK_H
