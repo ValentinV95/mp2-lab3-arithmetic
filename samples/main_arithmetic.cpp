af0fd3928bcb61eefdff408d0424d547db50f01b
NikolayGromov <43181570+NikolayGromov@users.noreply.github.com>
2018-11-27 18:14:46 +0300

    Add files via upload

--- samples/main_arithmetic.cpp
+++ samples/main_arithmetic.cpp
@@ -20,13 +20,12 @@ int main()
 	cout << "3)The integer and fractional parts of the number are separated '.' " << endl;
 	cout << "4)If the integer part of the number is 0, the point must be preceded by 0  " << endl;
 	cout << "5)if the fractional part of the number is 0 and you want to write not as an integer, then after the point should be 0  " << endl;
-	cout << "7)Sample must contain at least one action" << endl;
-	cout << "8)For example: 12.2/(-(11.2+1))"<< endl;
+	cout << "For example: 12.2/(-(11.2+1))"<< endl;
 	cout << "Enter your sample" << endl;
 	
 	cin >> c;
 	char* c1;
-	c1 = new char[c.length()];
+	c1 = new char[c.length()+1];
 	for (int i = 0;i < c.length();i++)
 	{
 		c1[i] = c[i];
