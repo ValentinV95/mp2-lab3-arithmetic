452fe7d125fc8d2c7d667d68c5f5d98f91f1f602
trixdade <roma.bezaev@yandex.ru>
2018-12-03 01:43:24 +0300

    Squashed commit of the following:
    commit 5e1a01f79319db20aff84de4af718a0eb8fc0c6a
    Author: trixdade <roma.bezaev@yandex.ru>
    Date:   Mon Dec 3 01:38:54 2018 +0300
    
        Add cpp
    
    commit 9a53f9fd0bae96f0371a0611c32ba3afa5f9d339
    Author: trixdade <roma.bezaev@yandex.ru>
    Date:   Mon Dec 3 01:35:15 2018 +0300
    
        Final Version

--- src/arithmetic.cpp
+++ src/arithmetic.cpp
@@ -1,44 +1,81 @@
-#include "C:/Users/Роман/Documents/Lab3/mp2-lab3-arithmetic/include/arithmetic.h"
+#include "../include/arithmetic.h"
 
-struct MINUS {
-	bool unary;
-	char operation = '-';
-};
-
-void Arithmetic::stringToTerm(string &expression) {
+void Arithmetic::stringToTerm(string &expression) 
+{
 	unsigned length = expression.length();
 	string stringForNumber;
 	double doubleNumber;
 	unsigned i = 0; 
 	while (i < length) {
 		if (expression[i] == ' ') i++; // пропускаем пробелы
-		if (expression[i] == '+' || expression[i] == '*' || expression[i] == '/') {
+		if (expression[i] == '+' || expression[i] == '*' || expression[i] == '/') { // обработка плюс, минус, деление
 			Term operation(expression[i]);
 			terms.push_back(operation);
 			i++;
 		}
-		else if (expression[i] == '-') { 
-			Term next(expression[i + 1]); // понадобиться в будущем
-			if (expression[i + 1] == '-') { // решение ситуации, когда много минусов подряд
-				int iterator = i; // чтобы не потерять нужную индексацию, создаем новый итератор
-				int count = 0;
-				while (expression[iterator] == '-') {
+		else if (expression[i] == '-') { // обработка минусов
+			bool unary = false;
+			int count = 1; // для подсчета минусов, один минус уже точно есть в expression[i]
+			string number; 
+
+			if (i == 0) unary = true;
+			
+			else if (expression[i - 1] == '(' || expression[i - 1] == '/' || expression[i - 1] == '*' || expression[i - 1] == '+') 
+			{
+				unary = true;
+			}
+			
+			if (i + 1 < length)	{
+
+				if (expression[i + 1] == '(') { // унарный минус перед скобкой обозначаем "_"
+					Term unary_minus('_');
+					terms.push_back(unary_minus);
+					i++;
+					continue;
+				}
+
+				while (expression[i + 1] == '-') { // обработка нескольких минусов подряд
 					count++;
+					i++;
 				}
-				if (!(count % 2)) { // если количество минусов четное, то можно заменить все на плюс
-					expression[i] = '+';
+				
+				if (count % 2 == 1 && !unary) {
+					Term a('-');
+					terms.push_back(a);
+					i++;
+					continue;
 				}
-				// если количество минусов нечетное, то просто оставляем минус и запробеливаем остальные
-				for (int k = i; k < iterator; k++) { // заменяем все ненужные минусы на пробелы, они все равно будут пропущены
-					expression[k] = ' ';
+				
+				if (count % 2 == 0 && !unary) {
+					Term a('+');
+					terms.push_back(a);
+					i++;
+					continue;
+				}
+
+				if (count % 2 == 1 && unary) {
+					number = "-";
+				}
+
+				if (expression[i + 1] >= '0' && expression[i + 1] <= '9') {
+					i++;
+					while (expression[i] >= '0' && expression[i] <= '9' || (expression[i] == '.')) { // обработка нецелых отрицательных чисел
+						number += expression[i];
+						i++;
+					}
+					doubleNumber = stof(number);
+					Term negativeNumber(doubleNumber);
+					terms.push_back(negativeNumber);
+					number.clear();
 				}
 			}
-			else if (next.getType() == true) { // если после минуса идет число 
+			else if (expression[i + 1] >= '0' && expression[i + 1] <= '9' || (expression[i + 1] == '.')) { // если после минуса идет число 
 				Term operation('-');
 				terms.push_back(operation);
+				i++;
 			}
 		}
-		else if (expression[i] >= '0' && expression[i] <= '9' || (expression[i] == '.')) {
+		else if (expression[i] >= '0' && expression[i] <= '9' || (expression[i] == '.')) { // обработка чисел
 			int pointCounter = 0;
 			while (expression[i] >= '0' && expression[i] <= '9' || (expression[i] == '.')) {
 				stringForNumber += expression[i];
@@ -47,7 +84,7 @@ void Arithmetic::stringToTerm(string &expression) {
 					pointCounter++;
 				}
 				if (pointCounter > 1) {
-					throw (string)"You have more than one point in the number";
+					throw "You have more than one point in the number";
 				}
 			}
 			doubleNumber = stof(stringForNumber);
@@ -63,7 +100,8 @@ void Arithmetic::stringToTerm(string &expression) {
 	}
 }
 
-void Arithmetic::termToPostfix() {
+void Arithmetic::termToPostfix() 
+{
 	unsigned size = terms.size();
 	vector <Term> postfix;
 	Stack <Term> operations;
@@ -72,25 +110,28 @@ void Arithmetic::termToPostfix() {
 			postfix.push_back(terms[i]); // помещаем число в выходную строку в виде Term
 		}
 		else if (terms[i].getType() == false) { // если встретилась операция
-		compare:
-			if (operations.isEmpty() || terms[i].priority() > operations.front().priority()) {
-				operations.push(terms[i]); //  помещаем операцию в стэк в виде Term
-			}
-			else if (terms[i].priority() <= operations.front().priority()) {
-				while (terms[i].priority() <= operations.front().priority()) {
-					postfix.push_back(operations.pop());
-				}
-				goto compare;
-			}
 			if (terms[i].getOperation() == '(') {
 				operations.push(terms[i]);
+				continue;
 			}
 			if (terms[i].getOperation() == ')') {
-				while (operations.isEmpty() && operations.front().getOperation() != '(') {
+				while (operations.getTop().getOperation() != '(') {
 					postfix.push_back(operations.pop());
 				}
+				operations.pop(); // pop '('
 			}
-			
+		compare:
+			if (operations.isEmpty() || operations.getTop().priority() < terms[i].priority()) {
+				if (terms[i].getOperation() != ')') {
+					operations.push(terms[i]); //  помещаем операцию в стэк в виде Term
+				}
+			}
+			else {
+				while (!operations.isEmpty() && operations.getTop().priority() >= terms[i].priority()) {
+					postfix.push_back(operations.pop());
+				} 
+				goto compare;
+			}
 			
 		}
 	}
@@ -100,16 +141,21 @@ void Arithmetic::termToPostfix() {
 	terms = postfix;
 }
 
-double Arithmetic::calculate() {
+double Arithmetic::calculate() 
+{
 	Stack <Term> stack;
-	Term termOperand1, termOperand2, stringRes;
+	Term termOperand1, termOperand2;
 	double operand1, operand2, res;
 
 	for (unsigned i = 0; i < terms.size(); i++) {
 		if (terms[i].getType() == true) {
 			stack.push(terms[i].getValue());
 		}
-		else {
+		else if (terms[i].getOperation() == '_') {
+			res = (-1)*stack.pop().getValue();
+			stack.push(res);
+			continue;
+		} else {
 			termOperand2 = stack.pop();
 			termOperand1 = stack.pop();
 			operand1 = termOperand1.getValue();
@@ -123,8 +169,12 @@ double Arithmetic::calculate() {
 			if (terms[i].getOperation() == '*')
 				res = operand1 * operand2;
 
-			if (terms[i].getOperation() == '/')
+			if (terms[i].getOperation() == '/') {
+				if (operand2 == 0) throw "Division by the zero";
 				res = operand1 / operand2;
+			}
+
+			
 
 			stack.push(res);
 		}
@@ -133,23 +183,33 @@ double Arithmetic::calculate() {
 	return result.getValue();
 }
 
-bool checkBrackets(const string &s) {
+bool checkBrackets(const string &s) 
+{
 	bool flag = true;
 	int count = 0, stringSize = s.length();
+
 	for (int i = 0; i < stringSize; i++) {
 		if (s[i] == '(') {
 			count++;
 			flag = false;
+			if (i < stringSize && s[i + 1] == '+' || s[i + 1] == '-' || s[i + 1] == '*' || s[i + 1] == '/') {
+				throw "Operation after opening bracket";
+			}
+			if (i > 0 && s[i - 1] >= '0' && s[i] <= '9') {
+				throw "Number before opening bracket";
+			}
 		}
 		if (s[i] == ')') {
 			count--;
 			flag = true;
 		}
 	}
+
 	return (flag == true && count == 0);
 }
 
-bool checkSymbols(const string &s) {
+bool checkSymbols(const string &s) 
+{
 	bool flag = false;
 	int stringSize = s.length(), allowedTermsSize = allowedTerms.length();
 	for (int i = 0; i < stringSize; i++) {
@@ -170,19 +230,32 @@ bool checkSymbols(const string &s) {
 	return true;
 }
 
-bool isCorrect(const string &s) {
+
+
+bool isCorrect(const string &s) 
+{
 	if (!checkBrackets(s)) {
-		throw (string)"Bracket in your expression are wrong";
+		throw "Bracket in your expression are wrong";
 	}
 	else if (!checkSymbols(s)) {
-		throw (string)"You should use only allowed symbols: 0123456789.()+-/* ";
+		throw "You should use only allowed symbols: 0123456789.()+-/* ";
+	}
+	else if (s[0] == '+' || s[0] == '-' || s[0] == '*' || s[0] == '/') {
+		throw "Operation is on the first place in the expression";
+	}
+	else if (s[s.length()-1] == '+' || s[s.length()-1] == '-' || s[s.length()-1] == '*' || s[s.length()-1] == '/') {
+		throw "Operation is at the end of the expression";
 	}
 	else return true;
 }
 
-int Term::priority() {
-	if ((operation == '+') || (operation == '-')) { return 1; }
-	else if ((operation == '*') || (operation == '/')) { return 2; }
-	else if ((operation == '(') || (operation == ')')) { return 0; }
-	else throw (string)"Priorty problem";
+
+
+int Term::priority() 
+{
+	if      (operation == '(' || operation == ')') { return 0; }
+	else if (operation == '+' || operation == '-') { return 1; }
+	else if (operation == '*' || operation == '/') { return 2; }
+	else if  (operation == '_')                    { return 3; }
+	else throw "Priority problem";
 }
