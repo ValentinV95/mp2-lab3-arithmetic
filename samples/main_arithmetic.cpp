4e0e9d86c4e73a384a3564c2e541767924b78462
arinavolkova <65176095+arinavolkova@users.noreply.github.com>
2020-12-25 20:20:04 +0300

    fix

--- samples/main_arithmetic.cpp
+++ samples/main_arithmetic.cpp
@@ -1,12 +1,12 @@
 // реализация пользовательского приложения
+ using namespace std;
 
 int main()
 {
- using namespace std;
 setlocale(LC_ALL, "Russian");
 string s;
-cout « "введите выражение" « endl;
-cin » s;
-cout «work(s) «endl;
+cout << "введите выражение" << endl;
+cin >> s;
+cout << work(s) <<endl;
 return 0;
 }
