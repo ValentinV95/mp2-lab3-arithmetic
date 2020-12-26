7bbab8dbf7715a06490f2aa2502eb6a2ae6d99a9
arinavolkova <65176095+arinavolkova@users.noreply.github.com>
2020-12-26 17:21:32 +0300

    Добавила тесты 2

--- test/test_stack.cpp
+++ test/test_stack.cpp
@@ -1,4 +1,35 @@
-// тесты для стека
+// ГІГҐГ±ГІГ» Г¤Г«Гї Г±ГІГҐГЄГ 
 
 #include "stack.h"
 #include <gtest.h>
+
+TEST(TStack, pop)
+{
+    TStack<int> s;
+    s.push(5);
+    ASSERT_NO_THROW(s.pop());
+}
+TEST(TStack, push)
+{
+    TStack<int> s;
+    ASSERT_NO_THROW(s.push(5));
+}
+TEST(TStack, get_number)
+{
+    TStack<int> s;
+    for (int i = 0; i < 11; i++)
+    {
+        s.push(i);
+    }
+    EXPECT_EQ(11, s.number());
+}
+TEST(TStack, change_size_stack)
+{
+    TStack<int> s(5);
+
+    for (int i = 0; i < 10; i++)
+    {
+        s.push(i);
+    }
+    EXPECT_EQ(7, s.pop());
+}
