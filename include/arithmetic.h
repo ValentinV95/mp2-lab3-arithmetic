51767176c3934637f7f489a5252a9895526a7eb5
Danil-Vasin <vasin.danil.2001@gmail.com>
2020-12-28 23:11:15 +0300

    labrab 3 vasin

--- include/arithmetic.h
+++ include/arithmetic.h
@@ -1,1 +1,231 @@
 // объ€вление функций и классов дл€ вычислени€ арифметических выражений
+#pragma once
+#include <string>
+#include "Stack.h"
+using namespace std;
+
+class Arithmetic
+{
+	string formula;
+	Stack <char> st_c;
+	Stack <double> st_d;
+	string infix;
+	string postfix;
+	int Arithmetic::Priority(char elem)
+	{
+		switch (elem)
+		{
+		case  '(': return 0;
+		case  ')': return 1;
+		case  '+': return 2;
+		case  '-': return 2;
+		case  '*': return 3;
+		case  '/': return 3;
+		case  '^': return 4;
+		}
+	}
+public:
+	Arithmetic() :st_c(10), st_d(10)
+	{
+		infix = "";
+		postfix = "";
+	}
+	Arithmetic(const Arithmetic& calc)
+	{
+		infix = calc.infix;
+		postfix = calc.postfix;
+		st_c = calc.st_c;
+		st_d = calc.st_d;
+	}
+	~Arithmetic() {};
+	bool Arithmetic::CheckBrackets()
+	{
+		st_c.Clear();
+		st_d.Clear();
+		for (int i = 0; i < infix.size(); i++)
+		{
+			if (infix[i] == '(')
+			{
+				if (st_c.Full())
+				{
+					return false;
+				}
+				else
+				{
+					st_c.Push('(');
+				}
+			}
+			if (infix[i] == ')')
+			{
+				if (st_c.Empty() == true)
+				{
+					return false;
+				}
+				else
+				{
+					st_c.Pop();
+				}
+			}
+		}
+		return st_c.Empty();
+	}
+
+	void Arithmetic::SetFormula(string str)
+	{
+		infix = "";
+		for (unsigned int i = 0; i < str.size(); i++)
+		{
+			if (str[i] == '+' || str[i] == '-' || str[i] == '*' || str[i] == '/' || str[i] == '^')
+			{
+				infix += " ";
+			}
+			infix += str[i];
+		}
+		if (CheckBrackets() != true)
+		{
+			throw 0;
+		}
+		Stack<char> s(infix.size());
+		Stack<double> d(infix.size());
+		st_c = s;
+		st_d = d;
+	}
+
+	string Arithmetic::GetInfix()
+	{
+		return infix;
+	}
+
+	string Arithmetic::GetPostfix()
+	{
+		return postfix;
+	}
+
+
+	void Arithmetic::SetExpression(string expr)
+	{
+		infix = expr;
+		if (CheckBrackets())
+		{
+			ToPostfix();
+		}
+	}
+
+
+	void  Arithmetic::ToPostfix()
+	{
+		if (!CheckBrackets())
+		{
+			throw "Wrong number of brackets";
+		}
+		st_c.Clear();
+		st_d.Clear();
+		postfix = " ";
+		string scr = "( " + infix + " )";
+		char elem = ' ! ';
+		unsigned int i = 0;
+		st_c.Clear();
+		while (i < scr.size())
+		{
+			if (scr[i] == '+' || scr[i] == '-' || scr[i] == '*' || scr[i] == '/' || scr[i] == '^')
+			{
+				postfix += " ";
+				elem = st_c.Pop();
+				while (Priority(elem) >= Priority(scr[i]))
+				{
+					postfix += elem;
+					elem = st_c.Pop();
+				}
+				st_c.Push(elem);
+				st_c.Push(scr[i]);
+			}
+			else
+				if (scr[i] == '(')
+				{
+					st_c.Push(scr[i]);
+				}
+				else
+					if (scr[i] == ')')
+					{
+						elem = st_c.Pop();
+						while (elem != '(')
+						{
+							postfix += elem;
+							elem = st_c.Pop();
+						}
+					}
+					else
+						if (scr[i] >= '0' && scr[i] <= '9')
+						{
+							postfix += scr[i];
+						}
+			i++;
+		}
+		if (!st_c.Empty())
+		{
+			throw 0;
+		}
+	}
+	double  Arithmetic::CalcPostfix()
+	{
+		if (!CheckBrackets())
+		{
+			throw "Wrong number of brackets";
+		}
+		st_d.Clear();
+		char* tmp;
+		unsigned int i = 0;
+		double res = 0;
+		while (i < postfix.size())
+		{
+			if (postfix[i] >= '0' && postfix[i] <= '9')
+			{
+				double d = strtod(&postfix[i], &tmp);
+				int j = tmp - &postfix[i];
+				i += j - 1;
+				st_d.Push(d);
+			}
+			if (postfix[i] == '+' || postfix[i] == '-' || postfix[i] == '*' || postfix[i] == '/' || postfix[i] == '^')
+			{
+				if (st_d.Empty())
+				{
+					throw "Wrong";
+				}
+				else
+				{
+					double op1, op2;
+					op2 = st_d.Pop();
+					op1 = st_d.Pop();
+					switch (postfix[i]) {
+					case '+':
+						res = op1 + op2; break;
+					case '-':
+						res = op1 - op2; break;
+					case '*':
+						res = op1 * op2; break;
+					case '/':
+						res = op1 / op2; break;
+					case '^':
+						res = pow(op1, op2); break;
+					default: if (formula[i] != ' ')
+						st_d.Push(formula[i] - '0');
+						break;
+					}
+					st_d.Push(res);
+				}
+			}
+			i++;
+		}
+		if (st_d.Empty())
+		{
+			throw "Wrong";
+		}
+		else
+		{
+			res = st_d.Pop();
+			return res;
+		}
+
+
+	}
+};
--- test/test_arithmetic.cpp
+++ test/test_arithmetic.cpp
@@ -1,3 +1,100 @@
 // тесты для вычисления арифметических выражений
 
-#include <gtest.h>
+#include "gtest.h"
+#include "arithmetic.h"
+
+TEST(Arithmetic, can_create_a_calculator)
+{
+    ASSERT_NO_THROW(Arithmetic calc);
+}
+TEST(Arithmetic, can_pass_a_string_to_the_calculator)
+{
+    Arithmetic calc;
+    std::string str = "1+5*2";
+    ASSERT_NO_THROW(calc.SetFormula(str));
+}
+TEST(Arithmetic, can_pass_the_correct_string_with_parentheses_to_the_calculator)
+{
+    Arithmetic calc;
+    std::string str = "(1+5)*2";
+    ASSERT_NO_THROW(calc.SetFormula(str));
+}
+
+TEST(Arithmetic, passing_a_formula_with_incorrectly_placed_parentheses_to_the_calculator_will_cause_an_exception)
+{
+    Arithmetic calc;
+    std::string str = "(1+5*2";
+    ASSERT_ANY_THROW(calc.SetFormula(str));
+}
+TEST(Arithmetic, can_calculate_the_result_of_an_arithmetic_expression)
+{
+    Arithmetic calc;
+    calc.SetFormula("1+5*2");
+    calc.ToPostfix();
+    ASSERT_NO_THROW(calc.CalcPostfix());
+}
+TEST(Arithmetic, evaluating_an_expression_with_incorrect_operations_will_throw_an_exception)
+{
+    Arithmetic calc;
+    calc.SetFormula("1+5*");
+    calc.ToPostfix();
+    ASSERT_ANY_THROW(calc.CalcPostfix());
+}
+
+
+TEST(Arithmetic, the_calculator_adds_integers_correctly)
+{
+    Arithmetic calc;
+    calc.SetFormula("1+5");
+    calc.ToPostfix();
+    double a = 1.0 + 5.0;
+    EXPECT_EQ(a, calc.CalcPostfix());
+}
+
+
+TEST(Arithmetic, the_calculator_subtracts_integers_correctly)
+{
+    Arithmetic calc;
+    calc.SetFormula("2-3");
+    calc.ToPostfix();
+    double a = 2.0 - 3.0;
+    EXPECT_EQ(a, calc.CalcPostfix());
+}
+
+
+TEST(Arithmetic, the_calculator_multiplies_integers_correctly)
+{
+    Arithmetic calc;
+    calc.SetFormula("3*7");
+    calc.ToPostfix();
+    double a = 3.0 * 7.0;
+    EXPECT_EQ(a, calc.CalcPostfix());
+}
+
+
+TEST(Arithmetic, the_calculator_divides_integers_correctly)
+{
+    Arithmetic calc;
+    calc.SetFormula("5/2");
+    calc.ToPostfix();
+    double a = 5.0 / 2.0;
+    EXPECT_EQ(a, calc.CalcPostfix());
+}
+
+
+TEST(Arithmetic, the_calculator_raise_to_a_degree_integers_correctly)
+{
+    Arithmetic calc;
+    calc.SetFormula("3^8");
+    calc.ToPostfix();
+    double a = pow(3.0, 8.0);
+    EXPECT_EQ(a, calc.CalcPostfix());
+}
+
+TEST(Arithmetic, the_calculator_correctly_counts_complex_arithmetic_expressions)
+{
+    Arithmetic calc;
+    calc.SetFormula("3*7/(2+5)-(10*4-39)");
+    calc.ToPostfix();
+    EXPECT_EQ(2, calc.CalcPostfix());
+}
